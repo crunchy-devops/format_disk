47bb07510a43af806ab758db132c851f74f70d1e
Herve <dockerlite@gmail.com>
2023-09-22 14:15:35 +0200

    adding line first step

--- filter_plugins/my_filters.py
+++ filter_plugins/my_filters.py
@@ -24,6 +24,14 @@ class FilterModule(object):
                     break
         return list_of_version
     def get_device(self, list_device):
-        return list_device
+        disk = []
+        device = []
+        flag = 0 
+        type_format = ['swap', 'ext4','xfs','dos', 'squashfs']
+        line = list_device.split('\n')
+        return line
+
+
+
 
 
